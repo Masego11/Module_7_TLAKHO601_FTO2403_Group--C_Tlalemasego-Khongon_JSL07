ce5a8bf126d82af76b23483bc1a21401b0520ef6
Tlalemasego <tlalemasego@gmail.com>
2024-05-30 11:38:10 +0200

    feat add -Coded my solution in the index.js file -Added styling to my css file -Added documentation to my readme file.

--- index.js
+++ index.js
@@ -12,21 +12,30 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage'); //Fetched the personalMessage from the Html document by ID
+      const courseNameInput = document.getElementById('courseName'); //Fetched the courseName from the Html document by ID
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
       const courseName = courseNameInput ? courseNameInput.value : "a course"; // Fallback to "a course" if no input
-  
+
       if (studentName.trim() === '' || personalMessage.trim() === '') {
         alert('Please fill in all fields');
         return;
       }
   
-      // 🚨 Generate certificate content dynamically
-      certificateContent. = `
+      // 🚨 Generate certificate content dynamically // Fetched certificate content using the .innerHtml element. Added certficate content 
+      certificateContent.innerHTML =  `  
+      <h2>Certifacte of Achievement</h2>
+      <p>This is to certfiy that</p>
       <h3>${studentName}</h3>
+      <p>Has almost completed</p>
+      <h3>${courseName}</h3>
+      <p>With legendary perserverance and world-class bad-assery for never giving up</p>
+      <img src='logo.png' alt='Codespace' max-width='60' height='60' >
+      <p>${personalMessage}</p>
+      
+      
     `;
     
       //  Display the modal
@@ -39,7 +48,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
+    closeModal.addEventListener('click', function () {
+      modal.style.display = 'none'; //set modal display to none
+      document.getElementById('modal').style.cursor ='pointer';
       
     });
   });
